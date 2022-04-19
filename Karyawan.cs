798cbfc33bb68814955e074981ee1d1a595c435a
Dion4286 <101573639+Dion4286@users.noreply.github.com>
2022-04-19 16:37:29 +0700

    ResponsiPemrograman4286
    Nama: Dionisius Rizky Pramusinto
    NIM: 21.11.4286

---
+++ Karyawan.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4286
+{
+    public class Karyawan
+    {
+        //constructor
+        public void Karyawan(string nik, string nama, int gajibulanan)
+        // properties
+        public string Nama{ get; set; }
+        public string Nik { get; set; }
+        public int GajiBulanan { get; set; }
+
+        //method
+        public void NikNama()
+        {
+            Console.WriteLine("{0}{1}{2}", Nama, Nik, GajiBulanan);
+        }
+
+        public void NaikGaji()
+        {
+            Console.WriteLine("Selamat Gaji Anda naik 10%");
+            int kenaikan;
+
+            kenaikan = GajiBulanan * 110;
+            GajiBulanan = kenaikan / 100;
+
+        }
+        public void SetNaikGaji()
+        {
+            Console.WriteLine("Nama/NIK          Gaji Bulanan");
+            Console.WriteLine("Nama / NIK           Gaji Bulanan");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace ResponsiPemrograman4286
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan kry1 = new Karyawan();
+            Karyawan kry2 = new Karyawan();
+
+
+            kry1.Nama = "Dion";
+            kry1.Nik = "12345";
+            kry1.GajiBulanan = 5000000;
+
+            kry2.Nama = "Adit";
+            kry2.Nik = "54321";
+            kry2.GajiBulanan = 3000000;
+
+
+            Console.WriteLine("No. Nama/NIK Gaji Bulanan");
+
+            Console.WriteLine("***************************");
+            Console.WriteLine("1"); kry1.NikNama();
+            Console.WriteLine("2"); kry2.NikNama();
+            Console.WriteLine();
+
+            kry1.NaikGaji();
+            kry2.NaikGaji();
+
+            Console.WriteLine("No. Nama/NIK Gaji Bulanan");
+            Console.WriteLine("***************************");
+            Console.WriteLine("1"); kry1.NikNama();
+            Console.WriteLine("2"); kry2.NikNama();
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
